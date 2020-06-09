55526b41451b96729001f8dca3e8db1c0e9fedd1
Sebastian Lipp <sebastian.lipp@ims.co.at>
2020-06-09 12:02:40 +0200

    Change botnet to multithreading and fix broken scan of agents

--- gns3/docker/bot-server/exec.py
+++ gns3/docker/bot-server/exec.py
@@ -5,12 +5,19 @@ import requests
 import re
 from random import randint
 from time import sleep
+from threading import Thread
+
+def run_cmd(agent, lines):
+    for line in lines:
+        url = base_url + '/api/' + agent + '/push'
+        myobj = {'cmdline': line}
+        sleep(randint(1,5))
+        requests.post(url, data=myobj, cookies=cookies)
 
 full_command = "./ares.py runserver -h 0.0.0.0 -p 8080 --threaded"
 subprocess.Popen(full_command, shell=True, close_fds=True)
 
 sleep(10)
-
 base_url = 'http://localhost:8080'
 
 while True:
@@ -25,19 +32,17 @@ while True:
     url = base_url + '/agents'
     x = requests.get(url, cookies=cookies)
     
-    matches = re.findall(r"checkbox_root_\d{15}", x.text)
+    matches = re.findall(r"value=\"root_.*?\"", x.text)
+
     agents = []
     for match in matches:
-        agents.append(match.replace("checkbox_",""))
-        
+        agents.append(match.replace("value=","").replace("\"",""))
+
     cmds_file = open('commands.txt', 'r') 
     lines = cmds_file.read().splitlines()
 
-    for line in lines:
-        for agent in agents:
-            url = base_url + '/api/' + agent + '/push'
-            myobj = {'cmdline': line}
-            sleep(randint(1,5))
-            requests.post(url, data=myobj, cookies=cookies)
+    for agent in agents:
+        t = Thread(target=run_cmd, args=(agent, lines,))
+        t.start()
 
     sleep(10)
